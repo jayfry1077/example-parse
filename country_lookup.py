d7dd2cb0dc1f0dbe2b4cb78fb519cfee48e1021e
Jonathan Bradbury <53982942+jayfry1077@users.noreply.github.com>
2021-07-16 21:52:29 -0700

    fix: exception handling

--- country_lookup.py
+++ country_lookup.py
@@ -14,10 +14,16 @@ def get_country_name(country_code=str):
     try:
         response = requests.get(COUNTRY_CODE_LOOKUP_URL, params=params).json()
 
+        # Check if response was good
         if response['api_status']['reply']['code'] != 200:
             return f'Unable to find country with provided code {country_code}'
 
+        # Check if our contry code was returned
+        if uppercase_country_code not in response['data'].keys():
+            return f'Invalid country code {country_code}'
+
+        # Return country code data
         return response['data'][uppercase_country_code]['name']
 
-    except Exception as e:
-        raise f'Error while getting country code {e}'
+    except:
+        return f'Error while getting country code {country_code}'
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import argparse
 from country_lookup import get_country_name
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument('lookup')
 parser.add_argument('--country_code', help='Takes in the country code')
 
